a1621338c704a52d31a5ba17833a9dd6e9f8e0af
Awerar <alexander.wahlsten@gmail.com>
2020-08-26 18:05:02 +0200

    More activity system implementation

--- lib/ActivitySystem/ActivityListener.dart
+++ lib/ActivitySystem/ActivityListener.dart
@@ -1,8 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:meetboard/ActivitySystem/ActivityReference.dart';
+import 'package:meetboard/ActivitySystem/ActivitySnapshot.dart';
 
-class ActivityListener with ChangeNotifier {
+class ActivityHandler with ChangeNotifier {
   final ActivityReference ref;
 
-  ActivityListener(this.ref);
+  DocumentSnapshot _lastDocumentSnapshot;
+  bool _listeningToUpdates = false;
+
+  ActivitySnapshot _latestSnapshot;
+  ActivitySnapshot get latestSnapshot => _latestSnapshot;
+
+  LocalValue<String> name;
+  LocalValue<DateTime> time;
+
+  ActivityHandler.fromFromOnline(this.ref) {
+    _listeningToUpdates = true;
+  }
+
+  ActivityHandler.fromData(this.ref, String name, DateTime time) {
+    this.name = LocalValue.value(this, name);
+    this.time = LocalValue.value(this, time);
+  }
+
+  void onActivityChanged() {
+    _latestSnapshot = _getLatestSnapshot();
+    notifyListeners();
+  }
+
+  ActivitySnapshot _getLatestSnapshot() {
+    if (_listeningToUpdates) {
+      return A
+    } else {
+
+    }
+  }
+
+  void _startListen() {
+    assert(!_listeningToUpdates);
+    _listeningToUpdates = true;
+  }
+
+  void _stopListen() {
+    assert(_listeningToUpdates);
+    _listeningToUpdates = false;
+  }
+}
+
+class LocalValue<T> {
+  final ActivityHandler _listener;
+  T _value;
+
+  T get value => _value;
+
+  LocalValue.value(this._listener, this._value);
+  LocalValue(this._listener);
+
+  void updateValue(T Function(T currentValue) modifier) {
+    T newValue = modifier(_value);
+    if (newValue != null) _value = newValue;
+  }
 }
--- lib/Models/ActivityListModel.dart
+++ lib/Models/ActivityListModel.dart
@@ -1,21 +1,73 @@
+import 'dart:collection';
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 import 'package:meetboard/ActivitySystem/ActivityListener.dart';
+import 'package:meetboard/ActivitySystem/ActivityPreviewSnapshot.dart';
 import 'package:meetboard/ActivitySystem/ActivityReference.dart';
 import 'package:meetboard/ActivitySystem/ActivitySnapshot.dart';
+import 'package:meetboard/Models/user_model.dart';
 
 typedef OnActivityChangeFunction = void Function(ActivitySnapshot snapshot);
 
 class ActivityListModel with ChangeNotifier {
   static ActivityListModel instance;
 
+  Map<ActivityReference, ActivityPreviewSnapshot> _previews = Map();
+  List<ActivityPreviewSnapshot> get previews => List<ActivityPreviewSnapshot>.unmodifiable(_previews.values);
+
   Map<ActivityReference, List<OnActivityChangeFunction>> _onChangeFunctions = Map();
-  Map<ActivityReference, ActivityListener> _activityListeners = Map();
+  Map<ActivityReference, ActivityHandler> _activityListeners = Map();
+
+  HashSet<ActivityReference> _globalActivities = HashSet(),
+      _localAddedActivities = HashSet(),
+      _localRemovedActivities = HashSet(),
+      _trackedActivities = HashSet();
+  HashSet<ActivityReference> get trackedActivities => HashSet.from(_trackedActivities);
+
+  ActivityListModel() {
+    UserModel.instance.userActivityCollection.snapshots().listen((querySnapshot) {
+      querySnapshot.documentChanges.forEach((change) {
+        ActivityReference ref = ActivityReference(change.document.documentID);
+
+        if (change.document.exists) {
+          //Added preview
+          _globalActivities.add(ref);
+          if(_localAddedActivities.contains(ref)) _localAddedActivities.remove(ref);
+        } else {
+          //Removed preview
+          _globalActivities.remove(ActivityReference(change.document.documentID));
+          if(_localRemovedActivities.contains(ref)) _localRemovedActivities.remove(ref);
+        }
+      });
+
+      onTrackedActivitiesChanged();
+    });
+  }
 
   ActivitySubscription listenForActivityChange(ActivityReference ref, OnActivityChangeFunction onChange) {
-    assert(_onChangeFunctions.containsKey(ref));
+    assert(trackedActivities.contains(ref));
 
     _onChangeFunctions[ref].add(onChange);
     return ActivitySubscription(() => _onChangeFunctions[ref].remove(onChange));
+  }
+
+  void onTrackedActivitiesChanged() {
+    HashSet<ActivityReference> prevTrackedActivities = _trackedActivities;
+    _trackedActivities = HashSet.from(_globalActivities)..removeAll(_localRemovedActivities)..addAll(_localAddedActivities);
+
+    Iterable<ActivityReference> newActivities = _trackedActivities.where((ref) => !prevTrackedActivities.contains(ref));
+    Iterable<ActivityReference> oldActivities = prevTrackedActivities.where((ref) => !trackedActivities.contains(ref));
+
+    newActivities.forEach((ref) {
+
+    });
+
+    oldActivities.forEach((ref) {
+
+    });
+
+    notifyListeners();
   }
 }
 
